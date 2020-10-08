7de47b73da8b17e41c446a114b8016655beabd9c
SkySails <malte.hallstrom@gmail.com>
2020-10-08 11:59:55 +0200

    Update meta tags

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -41,12 +41,29 @@ export default function PostTemplate({ content, data, slug }) {
         </title>
         <meta name="author" content={data.author} />
         <meta name="description" content={data.description} />
+
+        <meta name="title" content={`${data.title} | ${data.author}`} />
+        <meta name="description" content={data.description} />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content={`https://blog.hallstrom.dev/post/${slug}`}
+        />
         <meta property="og:title" content={data.title} />
         <meta property="og:description" content={data.description} />
-        <meta property="og:url" content={`/post/${slug}`} />
-        <meta property="og:site_name" content="Malte Hallström's Blog" />
         <meta property="og:image" content={data.img} />
-        <meta name="twitter:card" content={data.img_alt} />
+        <meta property="og:site_name" content="Malte Hallström's Blog" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content={`https://blog.hallstrom.dev/post/${slug}`}
+        />
+        <meta property="twitter:title" content={data.title} />
+        <meta property="twitter:description" content={data.description} />
+        <meta property="twitter:image" content={data.img} />
+        <meta name="twitter:creator" content="@malte_esaias"></meta>
       </Head>
       <ScrollProgress>
         <div id="bar" />
