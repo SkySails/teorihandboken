219340a757602bb2c0141e45e0affbe7c4ea5a74
SkySails <malte.hallstrom@gmail.com>
2020-10-09 10:30:16 +0200

    Add comments

--- src/layout/PostLayout.js
+++ src/layout/PostLayout.js
@@ -11,32 +11,26 @@ const PostContainer = styled.main`
   -ms-grid-columns: 1fr min(75ch, 100%) 1fr;
   grid-template-columns: 1fr min(75ch, 100%) 1fr;
   color: white;
-  padding: 5rem 0;
+  padding: 5rem 20px;
   position: relative;
 
   & > *:not(.full-bleed) {
     -ms-grid-column: 2;
     grid-column: 2;
-    padding: 0 20px;
   }
 
-  h1,
-  h2,
-  h3 {
-    font-family: "Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-      Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-    margin-top: 2em;
-    margin-bottom: 0;
-  }
-
+  /* Main post header  */
   .post-subject {
     margin-top: 10px;
   }
-
-  time {
-    opacity: 0.5;
+  /* All headings within the post itself  */
+  .content-header {
+    margin-bottom: 0;
+    padding-top: 50px;
+    margin-top: -20px;
   }
 
+  /* Those shiny looking inline-links!  */
   .markdown-link {
     position: relative;
     text-decoration: none;
@@ -84,6 +78,7 @@ const PostContainer = styled.main`
     }
   }
 
+  /* Normal post text  */
   p {
     font-family: "Roboto", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
       Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
@@ -92,49 +87,63 @@ const PostContainer = styled.main`
     color: #e5e5e5;
   }
 
-  img:not(.profile) {
-    max-height: 50vh;
-    /* max-width: 1000px; */
-    border-radius: 3px;
-    display: block;
-    width: 100%;
-    margin: auto;
-  }
-
+  /* Magical full-bleed image container  */
   .full-bleed {
     width: 100%;
     -ms-grid-column: 1;
     -ms-grid-column-span: 3;
     grid-column: 1 / 4;
     margin: 20px 0;
+
+    img {
+      max-height: 50vh;
+      /* max-width: 1000px; */
+      border-radius: 3px;
+      display: block;
+      width: 100%;
+      margin: auto;
+    }
   }
 
+  /* code snippets  */
   pre {
     position: relative;
     overflow: visible !important;
     margin: 2rem 0 !important;
+
+    &:before {
+      content: attr(data-title);
+      position: absolute;
+      top: -1.8em;
+      right: 10%;
+      padding: 0 10px;
+      padding-top: 0.3em;
+      border-top-left-radius: 7px;
+      border-top-right-radius: 7px;
+      height: 1.8em;
+      background: #282a36;
+      font-weight: bold;
+    }
   }
 
-  pre:before {
-    content: attr(data-title);
-    position: absolute;
-    top: -1.8em;
-    right: 10%;
-    padding: 0 10px;
-    padding-top: 0.3em;
-    border-top-left-radius: 7px;
-    border-top-right-radius: 7px;
-    height: 1.8em;
-    background: #282a36;
-    font-weight: bold;
-  }
-
-  ul {
-    margin: 0px 0 5px 40px;
+  .content-list {
+    margin: 0px 0 5px 10px;
     line-height: 1.8em;
+    list-style: none;
+
     li {
       font-size: 1.2em;
       margin: 10px 0;
+      position: relative;
+
+      &:before {
+        content: "• ";
+        color: #03e5a0;
+        position: absolute;
+        font-size: 1.5em;
+        left: -0.8em;
+        top: -0.1em;
+      }
     }
   }
 `;
