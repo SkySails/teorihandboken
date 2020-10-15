c6795aa3c9c723a9005ea905f04b27214c51c2f1
SkySails <malte.hallstrom@gmail.com>
2020-10-15 13:49:56 +0200

    Change structure

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import matter from "gray-matter";
 import moment from "moment";
 import styled from "styled-components";
 import Link from "next/link";
-import Search from "../src/components/Search";
+import Header from "../src/components/Header";
 
 export default function Home({ posts }) {
   return (
@@ -12,47 +12,62 @@ export default function Home({ posts }) {
       <Head>
         <title>Startsida</title>
       </Head>
-      <Search />
-      <h1>Blogginlägg</h1>
-      {posts.map(
-        ({ metadata: { title, description, date, snippet, tags }, slug }) => (
-          <Link href={`/post/[slug]`} as={`/post/${slug}`} key={title}>
-            <article>
-              <time>Posted {date}</time>
-              <header>
-                <div>
-                  <h2>{title}</h2>
-                  <div className="tags">
-                    {tags &&
-                      tags.map((tag) => (
-                        <span key={tag} className="tag">
-                          {tag}
-                        </span>
-                      ))}
+      <Header />
+      <Content>
+        <h1>Blogginlägg</h1>
+        {posts.map(
+          ({ metadata: { title, description, date, snippet, tags }, slug }) => (
+            <Link href={`/post/[slug]`} as={`/post/${slug}`} key={title}>
+              <article>
+                <time>Posted {date}</time>
+                <header>
+                  <div>
+                    <h2>{title}</h2>
+                    <div className="tags">
+                      {tags &&
+                        tags.map((tag) => (
+                          <span key={tag} className="tag">
+                            {tag}
+                          </span>
+                        ))}
+                    </div>
                   </div>
-                </div>
-              </header>
-              <p className="post-snippet">{snippet}</p>
-            </article>
-          </Link>
-        )
-      )}
+                </header>
+                <p className="post-snippet">{snippet}</p>
+              </article>
+            </Link>
+          )
+        )}
+      </Content>
     </Container>
   );
 }
 
-const Container = styled.main`
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 5em 1fr;
+  grid-template-areas:
+    "Header"
+    "Content";
+`;
+
+const Content = styled.main`
+  grid-area: Content;
   display: -ms-grid;
   display: grid;
   -ms-grid-columns: 1fr min(60ch, 100%) 1fr;
   grid-template-columns: 1fr min(60ch, 100%) 1fr;
   color: white;
-  padding-top: 2rem;
+  padding-top: 3rem;
 
   & > * {
     -ms-grid-column: 2;
     grid-column: 2;
     padding: 0 20px;
+  }
+
+  h1 {
   }
 
   h2 {
